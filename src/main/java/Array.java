0b002ce7eaeac892b6be01e47885bc58c3d01e06
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-01-29 10:25:32 +0100

    Add Array

---
+++ src/main/java/Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+
+        int[] numbers2 = {6, 7, 8, 9, 10};
+
+        System.out.println(numbers[2] + numbers[3]);
+    }
+}
