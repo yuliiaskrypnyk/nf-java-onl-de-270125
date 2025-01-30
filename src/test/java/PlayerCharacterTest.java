899de7a9674dfe4a59ed3115a50637ab0e6ee608
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-01-30 16:59:39 +0100

    Add PlayerCharacter

---
+++ src/test/java/PlayerCharacterTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerCharacterTest {
+    @BeforeEach
+    void resetAllXY() {
+        PlayerCharacter.x = 0;
+        PlayerCharacter.y = 0;
+    }
+
+    @Test
+    void getX_ShouldReturn0_WhenStart() {
+        int expected = 0;
+        int actual = PlayerCharacter.getX();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getY_ShouldReturn0_WhenStart() {
+        int expected = 0;
+        int actual = PlayerCharacter.getY();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void move_ShouldReturn1_WhenW() {
+        int actual = PlayerCharacter.move("W");
+        Assertions.assertEquals(1, actual);
+    }
+
+    @Test
+    void move_ShouldReturn1_WhenS() {
+        int actual = PlayerCharacter.move("S");
+        assertEquals(-1, actual);
+    }
+
+    @Test
+    void move_ShouldReturn1_WhenD() {
+        int actual = PlayerCharacter.move("D");
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void move_ShouldReturn1_WhenA() {
+        int actual = PlayerCharacter.move("A");
+        assertEquals(-1, actual);
+    }
+}
