b530188fd4c3c32ae609c518b0f501513fcb652f
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-01-30 09:56:30 +0100

    method calculate returns 1 when number 1

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,17 +1,5 @@
 public class FizzBuzz {
-    public static String answer(int person) {
-        if (person % 15 == 0) {
-            return "Fizz Buzz";
-        }
-
-        if (person % 3 == 0) {
-            return "Fizz";
-        }
-
-        if (person % 5 == 0) {
-            return "Buzz";
-        }
-
-        return String.valueOf(person);
+    public static String calculate(int number) {
+        return  "1";
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,75 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class FizzBuzzTest {
 
-    @ParameterizedTest
-    @CsvSource(
-        delimiter = '=',
-        value = {
-                "1=1",
-                "2=2",
-                "3=Fizz",
-                "4=4",
-                "5=Buzz",
-                "15=Fizz Buzz"
-        }
-    )
-
-    void test_answer(int person, String expected) {
-        String actual = FizzBuzz.answer(person);
-        Assertions.assertEquals(expected, actual);
-    }
-
-/*    @Test
-    void expeect2_whenPerson2() {
-        int person = 2;
-
-        String actual = FizzBuzz.answer(person);
-
-        String expected = "2";
-        Assertions.assertEquals(expected, actual);
-    }
-
     @Test
-    void expeectFizz_whenPerson3() {
-        int person = 3;
+    void expect1_whenGivenNumber1() {
+        // Given
+        int number = 1;
+        // When
+        String actual = FizzBuzz.calculate(number);
+        // Then
+        String expected = "1";
 
-        String actual = FizzBuzz.answer(person);
-
-        String expected = "Fizz";
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void expeect4_whenPerson4() {
-        int person = 4;
-
-        String actual = FizzBuzz.answer(person);
-
-        String expected = "4";
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void expeectBuzz_whenPerson5() {
-        int person = 5;
-
-        String actual = FizzBuzz.answer(person);
-
-        String expected = "Buzz";
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void expeectFizzBuzz_whenPerson15() {
-        int person = 15;
-
-        String actual = FizzBuzz.answer(person);
-
-        String expected = "Fizz Buzz";
-        Assertions.assertEquals(expected, actual);
-    }*/
 }
